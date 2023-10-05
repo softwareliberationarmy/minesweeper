948283c4a969047081cf971bff838435d92c0eda
Kerry Patrick <kpatrick@mmmicrosystems.com>
2023-10-05 08:01:43 -0500

    hardcoding 2x2  layout and starting to apply styling

--- src/Minefield.tsx
+++ src/Minefield.tsx
@@ -1,5 +1,17 @@
 import React from 'react';
+import './Minefield.css';
 
 export const Minefield = () => {
-  return <div id="minefield">minefield goes here</div>;
+  return (
+    <div id="minefield">
+      <div className="row">
+        <button className="cell"></button>
+        <button className="cell"></button>
+      </div>
+      <div className="row">
+        <button className="cell"></button>
+        <button className="cell"></button>
+      </div>
+    </div>
+  );
 };
