5b55b6d39ea5267bbbc32433ccec8c28cf022d85
Kerry Patrick <kpatrick@mmmicrosystems.com>
2023-10-13 21:08:38 -0500

    users can now flag cells they think have a bomb

--- README.md
+++ README.md
@@ -5,4 +5,3 @@ You know it, you love it, you wish there was a React.JS version.
 ## TODO
 
 - evaluate win/loss status and report it to the user
-- allow user to right-click and flag cells
--- src/MinefieldCell.tsx
+++ src/MinefieldCell.tsx
@@ -1,6 +1,7 @@
-import { useEffect, useState } from 'react';
-import { CellReveal } from './CellReveal';
-import './MinefieldCell.css';
+import { useEffect, useState } from "react";
+import { CellReveal } from "./CellReveal";
+import "./MinefieldCell.css";
+import { Icon } from "semantic-ui-react";
 
 interface MinefieldCellProps {
   bombCount: number;
@@ -16,15 +17,33 @@ export const MinefieldCell = ({
   };
 
   const [reveal, setReveal] = useState(false);
+  const [showFlag, setShowFlag] = useState(false);
 
   useEffect(() => {
     setReveal(false);
   }, [lastRunDate]);
 
+  const onRightClickButton = (
+    event: React.MouseEvent<HTMLButtonElement, MouseEvent>
+  ): void => {
+    event.preventDefault();
+    setShowFlag((current) => !current);
+  };
+
   const inner = reveal ? (
     <CellReveal bombCount={bombCount} />
   ) : (
-    <button className="cell" onClick={userReveal}></button>
+    <button
+      className="cell"
+      onClick={userReveal}
+      onContextMenu={onRightClickButton}
+    >
+      {showFlag ? (
+        <Icon name="flag" size="large" style={{ color: "red" }} />
+      ) : (
+        ""
+      )}
+    </button>
   );
 
   return <td className="cell">{inner}</td>;
